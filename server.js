c1d4d2a8a3fb8ddeddc1009d9a6ba1f4c4a23ed0
Trung Van Huynh <Trung.Huynh@vn-m-2815.local>
2026-08-11 22:32:13 +0700

    Add Express backend for OpenAI proxy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+app.use(express.static(__dirname));   // ← add this line
 app.use(express.json());
 app.use('/api/generate-script', cors({ origin: '*' }));
 
